776e65f2df3e5a532bf93665740b5d4739011703
unknown <jabri.oldd@gmail.com>
2022-12-08 00:47:56 +0100

    setting up the backend

--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 /* Configurations */
 dotenv.config();
 const app = express();
@@ -17,7 +20,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 /*Routes */
-app.use("/client", cientRoutes);
+app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
-app.use("sales", salesRoutes);
+app.use("/sales", salesRoutes);
+app.get("/", (req, res) => {
+  res.send("aqzdf");
+});
+/*Mongoose Setup */
+const PORT = process.env.PORT || 5050;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`lestining to the port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
